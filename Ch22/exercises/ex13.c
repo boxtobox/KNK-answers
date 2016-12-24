5ca00e455ae7c060719f8ab716d27ce3c7d6859e
boxtobox <grandnex@gmail.com>
2016-12-24 14:49:56 +0900

    completed to ex16

--- Ch22/exercises/ex13.c
+++ Ch22/exercises/ex13.c
@@ -2,29 +2,73 @@
 #include <stdlib.h>
 
 int line_length(const char *filename, int n);
+void print_file_line_len(const char *filename);  // for reference
 
 int main(void)
 {
     FILE *fp;
-    char *filename = "ex12_test.txt";
+    char *filename = "ex13_test.txt";
     int n;
-    printf("Enter a line number: ");
+    printf("enter a line number: ");
     fscanf(stdin, "%d", &n);
-    int len = line_length(filename, n);
 
+    int len = line_length(filename, n);
     printf("length of line %d: %d\n", n, len);
+
+    print_file_line_len(filename);
 
 }
 
 int line_length(const char *filename, int n)
 {
-    int ch, i = 1, len = 0;
+    int ch, line, len = 0;
     FILE *fp;
     if ((fp = fopen(filename, "r")) == NULL) {
         printf("Error: cannot open %s\n", filename);
         exit(EXIT_FAILURE);
     }
-    while ((ch = fgetc(fp) != EOF)) {
-        printf("line %d\n", i);
+
+    for (line = 1; line < n; ) {
+        ch = fgetc(fp);
+        if (ch == EOF)
+            return 0;
+        if (ch == '\n')
+            line++;
+    }
+    // we now reached line n
+    for(;;) {
+        ch = fgetc(fp);
+        if (ch == EOF || ch == '\n')
+            return len;
+        len++;
+    }
+}
+
+void print_file_line_len(const char *filename)
+{
+    int ch, line = 1, len = 0;
+    FILE *fp;
+    if ((fp = fopen(filename, "r")) == NULL) {
+        printf("Error: cannot open %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%02d: ", line);
+    for(;;) {
+        ch = fgetc(fp);
+        if (ch == EOF) {
+            printf(" (len: %d)\n", len);
+            return;
+        }
+        else if (ch == '\n') {
+            printf(" (len: %d)\n", len);
+            line++;
+            printf("%02d: ", line);
+            len = 0;
+        }
+        else {
+            printf("%c", ch);
+            len++;
+        }
     }
 }
